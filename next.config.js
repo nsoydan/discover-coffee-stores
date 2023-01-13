8eaed3ab3ebf218b52d212899d30d0d347256ec6
nihat soydan <nsoydan@gmail.com>
2023-01-13 23:42:41 +0300

    experimental layer error is fixed

--- next.config.js
+++ next.config.js
@@ -2,12 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
   images: {
     domains: ["images.unsplash.com", "ss3.4sqi.net"],
   },
+
   webpack: (config) => {
     config.experiments = {
       topLevelAwait: true,
+      layers: true,
     };
     return config;
   },
